d87cab369fd247adf0996c606aaca2fa1a22c2f2
mahdiyeh <mahdiyehhaj497@gmail.com>
2025-12-24 14:03:19 +0330

    edit code

--- input-app/src/Components/TodoApp/TodoApp.jsx
+++ input-app/src/Components/TodoApp/TodoApp.jsx
@@ -1,36 +1,49 @@
 import { useState } from "react"
 import TodoItem from "../Todoitem/TodoItem"
 
-
 export default function TodoApp() {
+    const [todos, setTodos] = useState([])
     const [inputVal, setInputVal] = useState("")
 
-    const [todos, setTodos] = useState([])
-    
     function handleAddTodo() {
-        const newTodo = {
-            id: Math.floor(Math.random() * 100000),
+        if (inputVal.trim() === "") return
+       const newTodos = {
+            id: Math.floor(Math.random() * 10000),
             title: inputVal,
-            is_done:false
-        } 
-        setTodos([...todos,newTodo])
+        }
+        setTodos([...todos,newTodos])
+        setInputVal("")
     }
-    function handleDeleteTodo() {
-       setInputVal("") 
+   
+    function handleDeleteTodo(id) {
+        setTodos(todos.filter(todo =>todo.id !==id))
     }
-    function handleEdit() {
+    function handleOnEdit(id ,newTitle) {
+        setTodos(todos.map(todo => todo.id ===id ? {...todo ,title:newTitle}: todo))
+    }
         
-        setInputVal("please fill up that")
-    }
+       
+            
+        
+    
     return (
-      <>
-        <input value={inputVal}  onChange={(e)=>setInputVal(e.target.value)} type="text" />
-            <button onClick={handleAddTodo}>add todo</button>
-            <button onClick={handleDeleteTodo}>delete addtodo</button>
-            <button onClick={handleEdit}>Edite me</button>
+        <> 
+            <input
+                type="text"
+                value={inputVal}
+                onChange={evt => setInputVal(evt.target.value)}
+            />
+            <button onClick={handleAddTodo}>add Todo</button>
             <ul>
-                {todos.map(todo => <TodoItem key={todo.id } title={todo.title} />)}
+                {todos.map(todo => <TodoItem key={todo.id}
+                    title={todo.title}
+                    id={todo.id}
+                    onDelete={handleDeleteTodo}
+                    onEdit={handleOnEdit}
+                />)}
             </ul>
-      </>
-    );
+        </>
+    )
 }
+
+
--- input-app/src/Components/TodoItem/TodoItem.jsx
+++ input-app/src/Components/TodoItem/TodoItem.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 
 
 
-export default function TodoItem({ title }) {
+export default function TodoItem({ title, id, onEdite, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editValue, setEditValue] = useState(title)
 
-    return (
-      <>
-        <li>{title }</li>
-            
-      </>
-    );
- }
+  function handleSave() {
+    if (editValue.trim() === "") return
+    onEdite(id, editValue)
+    setIsEditing(false)
+  }
+  return (
+    <li>
+      {isEditing ? (
+        <>
+          <input type="text" value={editValue} onChange={evt => setEditValue(evt.target.value)} />
+          <button onClick={handleSave}>save</button>
+        </>
+      ) : (
+        <>
+          {title}
+          <button onClick={() => setIsEditing(true)}>edit</button>
+        </>
+      )}
+      <button onClick={() => onDelete(id)}>delete</button>
+    </li>
+  );
+}
